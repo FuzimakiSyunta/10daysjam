e2a6b25e28dd9d469e43e6819f133d25c5850f2d
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-09-11 22:23:07 +0900

    map

--- DirectXGame/Bomm.cpp
+++ DirectXGame/Bomm.cpp
@@ -3,26 +3,24 @@
 #include <TextureManager.h>
 
 void Bomm::Initialize() {
-	srand((unsigned int)time(nullptr));
-	// 座標
-	pos = {center.x - 8, center.y - 160};
-
 	BomsTexture_ = TextureManager::Load("Landmine.png");
-	bomsSprite_.reset(Sprite::Create(BomsTexture_, pos));
+	
 }
 
 void Bomm::Update(float scroll_) {
 	scroll = scroll_ * -1;
-	pos.y = scroll;
-	// これだと1から6の間
-	number = rand() % 6 + 1;
-	// 画像の座標更新
-	bomsSprite_->SetPosition(pos);
-	////これだと1から6の間
-	// number = rand() % 6 + 1;
+	
+	
+
+	
 }
 
 void Bomm::Draw() {
-
-	bomsSprite_->Draw(); 
+	for (int y = 0; y < MapCountX; y++) {
+		for (int x = 0; x < MapCountX; x++) {
+			if (map[y][x] == 0) {
+				bomsSprite_->Draw();
+			}
+		}
+	}
 }
