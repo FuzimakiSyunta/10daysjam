e0dcc9cc6ad3bc601ca12138fad6b952fda516d6
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-09-12 09:56:42 +0900

    l

--- DirectXGame/Bomm.cpp
+++ DirectXGame/Bomm.cpp
@@ -3,15 +3,22 @@
 #include "ImGuiManager.h"
 
 void Bomm::Initialize() { 
-	
+	sizeY =(int) map.size();
+	sizeX = (int)map[0].size();
 	// テクスチャ
 	BomsTexture_ = TextureManager::Load("Landmine.png");
 	bomsSprite_.reset(Sprite::Create(BomsTexture_, pos[i]));
 	
 }
 
-void Bomm::Update() { 
-	 
+void Bomm::Update() {
+	for (int y = 0; y < easymap.size(); y++) {
+		// 列の描画
+		for (int x = 0; x < nomalmap.size(); x++) {
+			if (map[y][x] == NORMAL) {
+			}
+		}
+	}
 }
 	
 
